2cd5960ce2593cece3484a4b7590f02682f387c8
Carlos Ariza <cag19@correo.ugr.es>
2018-10-11 00:56:02 +0200

    Actualizo requirements.txt

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
 from fileS import devuelveTrue
-import unittest
 
-class SoloTest(unittest.TestCase):
-
-    def testTrue(self):
-        self.assertTrue(devuelveTrue(), "devuelveTrue devuelve True")
-
-if __name__ == '__main__':
-    unittest.main()
+def testTrue():
+    assert devuelveTrue() == True
