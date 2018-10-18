f7f41961b6f6f465ae4fb09132e3b93f29f45f41
Carlos Ariza <cag19@correo.ugr.es>
2018-10-18 23:40:15 +0200

    Pruebas en app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Worldddddddddd!"
+    return "Prueba de despliegue para el proyecto de Infraestructura Virtual"
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
