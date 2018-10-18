f2a5b1a10b8a96724110f8e74d5a69a2a9bd6b44
Carlos Ariza <cag19@correo.ugr.es>
2018-10-18 23:00:54 +0200

    Añado ficheros para despliegue en Heroku

---
+++ app.py
@@ -0,0 +1,10 @@
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello Worldddddddddd!"
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
