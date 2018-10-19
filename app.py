8484c82a2cba52235ff6bc5035d6f7124f8b9b81
Carlos Ariza <cag19@correo.ugr.es>
2018-10-19 10:22:01 +0200

    Añado funcionalidad status

--- app.py
+++ app.py
@@ -5,5 +5,16 @@ app = Flask(__name__)
 def hello():
     return "Prueba de despliegue para el proyecto de Infraestructura Virtual"
 
+@app.route("/status")
+def status():
+    f=open("status.json", "r")
+    data = f.read()
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
