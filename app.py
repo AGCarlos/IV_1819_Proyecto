3d78d63353528e0392736ddaa196904629bbd22f
Carlos Ariza <cag19@correo.ugr.es>
2018-10-19 10:50:45 +0200

    Actualizo aplicación para JSON

--- app.py
+++ app.py
@@ -9,12 +9,7 @@ def hello():
 def status():
     f=open("status.json", "r")
     data = f.read()
-    response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
