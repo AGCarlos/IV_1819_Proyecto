c4fb032bb1661d9b9d215b4795c31cfedd7dfb6f
Carlos Ariza <cag19@correo.ugr.es>
2018-10-22 11:42:55 +0200

    Añado templates

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
 @app.route("/status")
 def status():
     f=open("status.json", "r")
