dd5cfe7fe8c6e859b362e4e6b76fe2ff8a57a823
Carlos Ariza <cag19@correo.ugr.es>
2018-11-07 13:08:52 +0100

    Actualizo aplicación

--- app.py
+++ app.py
@@ -34,30 +34,30 @@ def ejemplo():
 @app.route("/addFiles")
 def add():
 
-jsonf = {
-    "id": "1",
-    "nombre": "pepe",
-    "path": "https://i.ytimg.com/vi/ADYSC-5OWVM/maxresdefault.jpg",
-    "user": "fernando",
-    "fecha": str(datetime.datetime.now()),
-    "type": "img",
-    "format": "jpg"
-}
-#conectar a Redis y obtener el siguiente ID
-r = redis.Redis()
-cont = int(str(r.get("cont"))[2:-1])
-fl.createFile(cont + 1,jsonf)
+    jsonf = {
+        "id": "1",
+        "nombre": "pepe",
+        "path": "https://i.ytimg.com/vi/ADYSC-5OWVM/maxresdefault.jpg",
+        "user": "fernando",
+        "fecha": str(datetime.datetime.now()),
+        "type": "img",
+        "format": "jpg"
+    }
+    #conectar a Redis y obtener el siguiente ID
+    r = redis.Redis()
+    cont = int(str(r.get("cont"))[2:-1])
+    fl.createFile(cont + 1,jsonf)
 
-jsonf = r.get(2).decode('utf8').replace("'", '"')
-# Load the JSON to a Python list & dump it back out as formatted JSON
-data = json.loads(jsonf)
-return json.dumps(data, indent=4, sort_keys=True)
-response = app.response_class(
-    response=json.dumps(data, indent=4, sort_keys=True),
-    status=200,
-    mimetype='application/json'
-)
-return response
+    jsonf = r.get(2).decode('utf8').replace("'", '"')
+    # Load the JSON to a Python list & dump it back out as formatted JSON
+    data = json.loads(jsonf)
+    return json.dumps(data, indent=4, sort_keys=True)
+    response = app.response_class(
+        response=json.dumps(data, indent=4, sort_keys=True),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
