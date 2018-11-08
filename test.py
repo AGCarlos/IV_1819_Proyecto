68752b397b6c318527beec98abcfe6f7c40f2388
Carlos Ariza <cag19@correo.ugr.es>
2018-11-08 20:14:49 +0100

    Modificados test debido a cambios en funciones

--- test.py
+++ test.py
@@ -9,8 +9,10 @@ class TestClass(object):
 
     #Check checkUser
     def test_checkUser(self):
-        assert fl.checkUser("lamslf") == "OK"
-        fl.createFile(1,"{'lmao':'lmao'}")
+        fl.createFile("archivo",{'lmao':'lmao1'})
+        r = redis.Redis()
+
+        assert fl.checkUser("archivo") == "OK"
         assert fl.checkUser(1) == "None"
         assert fl.checkUser(True) == "None"
         assert fl.checkUser(1.0) == "None"
@@ -18,29 +20,32 @@ class TestClass(object):
     #Check createFile
     def test_userCreated(self):
         r = redis.Redis()
-        assert fl.createFile(1,"{'lmao':'lmao'}") == 'OK'
-        assert fl.createFile("{'lmao':'lmao'}",1,) == 'None'
-        assert fl.createFile("{'lmao':'lmao'}","{'lmao':'lmao'}") == 'None'
-        assert fl.createFile(True,"{'lmao':'lmao'}") == 'None'
-        assert fl.createFile("{'lmao':'lmao'}",True) == 'None'
+        fl.createFile("archivo",{'lmao':'lmao1'})
+        assert fl.createFile({'lmao':'lmao'},"lmao") == 'None'
+        assert fl.createFile({'lmao':'lmao'},1) == 'None'
+        assert fl.createFile(1,{'lmao':'lmao'}) == 'None'
+        assert fl.createFile(True,{'lmao':'lmao'}) == 'None'
+        assert fl.createFile({'lmao':'lmao'},True) == 'None'
+        assert fl.createFile(1,True) == 'None'
 
     #Check if file is created correctly
     def test_userCreatedCorrect(self):
         r = redis.Redis()
-        fl.createFile(1,"{'lmao':'lmao'}")
-        assert str(r.get(1)) != "None"
-        assert str(r.get("userasdas:psadasasswd")) == 'None'
+        fl.createFile("archivo",{'lmao':'lmao1','lmao2':'lmao3'})
+        assert str(r.hmget("archivo","lmao"))[3:-2] == 'lmao1'
+        assert str(r.hmget("archivo","lmao2"))[3:-2] == 'lmao3'
+        assert str(r.hmget("archivo","otracosa"))[1:-1] == 'None'
 
     #Check deleteFile
     def test_userDeletedCorrect(self):
         r = redis.Redis()
-        assert fl.deleteFile(1) == 'OK'
-        assert fl.deleteFile("passwd") == 'None'
+        fl.createFile("archivo",{'lmao':'lmao'})
+        assert fl.deleteFile("leler8") == 'OK'
         assert fl.deleteFile(True) == 'None'
-
+        assert fl.deleteFile(1) == 'None'
     #Check if file is deleted correctly
     def test_userDeleted(self):
         r = redis.Redis()
-        fl.createFile(1,"{'lmao':'lmao'}")
-        fl.deleteFile(1)
+        fl.createFile("archivo",{'lmao':'lmao'})
+        fl.deleteFile("archivo")
         assert str(r.get(1)) == "b'None'"
