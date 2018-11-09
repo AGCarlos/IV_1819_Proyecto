40f19e81555c62d58ccf6942e52063867b1964c6
Carlos Ariza <cag19@correo.ugr.es>
2018-11-09 13:15:21 +0100

    Pruebas de conexion

--- fileS.py
+++ fileS.py
@@ -16,7 +16,7 @@ class FileDownload:
             r = redis.Redis()
             return r
         else:
-            r = redis.from_url(os.environ.get(bytes("redis://h:p90d63f49afeefa17c338b0345f951b144e3f5e9bd649b4a2e625c3513fd1a5c6@ec2-52-54-174-93.compute-1.amazonaws.com:42199")))
+            r = redis.from_url(os.environ.get("redis://h:p90d63f49afeefa17c338b0345f951b144e3f5e9bd649b4a2e625c3513fd1a5c6@ec2-52-54-174-93.compute-1.amazonaws.com:42199"))
             return r
 
     def checkUser(self,user):
@@ -31,7 +31,7 @@ class FileDownload:
         if type(user) != str:
             return "None"
         else:
-            r = self.conexion("d")
+            r = self.conexion("l")
             archivo = str(r.get(1))
             #if archivo == "b'None'":
                 #print("El user "+user+" no tiene ningun archivo pendiente")
@@ -70,7 +70,7 @@ class FileDownload:
         if type(id) != str or type(json) != type(dict()):
             return "None"
         else:
-            r = self.conexion("d")
+            r = self.conexion("l")
             r.delete(id)
             r.hmset(id, json)
             return "OK"
@@ -84,7 +84,7 @@ class FileDownload:
         if type(id) != str:
             return "None"
         else:
-            r = self.conexion("d")
+            r = self.conexion("l")
             r.delete(id)
             return "OK"
 
